22807b7fa5f38e8d843fd437f9a89d7b66f2d1d0
jsonmake <1293223141@qq.com>
2020-06-15 17:34:24 +0800

    考核部分部分组件

---
+++ src/components/table-solt/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <slot v-bind:itemValue = "todoStr" >
+      {{todoStr}}
+    </slot>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      //数组
+      List: {
+        type: Object,
+        default: null
+      },
+      //下标
+      num:{
+        type:Number,
+        default:0
+      }
+    },
+    data () {
+      return {
+        todoList:this.List,
+        todoNum:this.num,
+        todoStr:''
+      }
+    },
+    created(){
+      var numStr = 'kbiId' + this.todoNum
+      this.todoStr = this.todoList[numStr]
+    },
+    methods: {
+    }
+  }
+</script>
+
+
