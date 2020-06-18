488db55ab082751dcae32d5ffdfa01c75c92e93f
jsonmake <1293223141@qq.com>
2020-06-18 09:00:48 +0800

    quickinput.vue second change commit

--- src/components/quickinput-module/quickinput.vue
+++ src/components/quickinput-module/quickinput.vue
@@ -1,59 +1,55 @@
 <template>
-  <div>
-    <van-row type="flex" justify="center" class="mtmb5">
-      <van-col span="21">
-       <van-row type="flex" justify="center">
-           <van-col span="18">
-                <div class="uls">
-          <div style="width:80%;">
-            <button class="execbtn" style="width:90%;" @click="visibletrue()">{{title}}</button>
+  <div @click="visibletrue()">
+    <van-row type="flex" justify="center">
+      <van-col span="14">
+        <div class="uls">
+          <div>
+            <button class="execbtn">{{title}}</button>
           </div>
           <div style="margin-right:5px;">
-            <van-icon name="arrow-down" @click="visibletrue()"/>
+            <van-icon name="arrow-down"/>
           </div>
         </div>
-           </van-col>
-       </van-row>
       </van-col>
     </van-row>
   </div>
 </template>
 
 <script>
-export default {
-  props: ['title'],
-  data: function () {
-    return {
+  export default {
+    props: ['title'],
+    data: function () {
+      return {
 
-    }
-  },
-  watch: {
+      }
+    },
+    watch: {
 
-  },
-  methods: {
-    visibletrue () {
-      this.$emit('vtrue')
-    }
-  },
-  mounted () {}
-}
+    },
+    methods: {
+      visibletrue () {
+        this.$emit('vtrue')
+      }
+    },
+    mounted () {}
+  }
 </script>
 
 <style scoped>
 
-.execbtn {
-  font-size: 14px;
-  background: #fff;
-  color: #777777;
-  line-height: 35px;
-  flex: 1;
-  border: 0;
-  height: 30px;
-}
-.uls{
-border-bottom:1px solid rgb(0,0,0); height:30px;width:100%; display:flex; align-items:flex-end;justify-content:space-between;
-}
-.mtmb5{
-margin-top:5px;margin-bottom:5px;
-}
+  .execbtn {
+    font-size: 14px;
+    background: #fff;
+    color: #777777;
+    line-height: 35px;
+    flex: 1;
+    border: 0;
+    height: 30px;
+  }
+  .uls{
+    margin-top:5px;margin-bottom:5px;border-bottom:1px solid rgb(0,0,0); height:30px;width:100%; display:flex; align-items:flex-end;justify-content:space-between;
+  }
+  .mtmb5{
+    margin-top:5px;margin-bottom:5px;
+  }
 </style>
